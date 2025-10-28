5bcea7f2d40f52e396a404ef2c6d0ae9dd159078
Aaron Richards <aaronrichards46@gmail.com>
2025-10-28 15:54:36 +0000

    added login_manager user loader, fixed route methods as I forgot [] and added error handling route

--- app.py
+++ app.py
@@ -20,27 +20,31 @@ with app.app_context():
     db.init_app(app)
     db.create_all()
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods="GET")
+@app.route("/login", methods=["GET"])
 def login_page():
     return render_template("login.html")
 
-@app.route("/login", methods="POST")
+@app.route("/login", methods=["POST"])
 def login_action():
     return render_template("index.html")
 
-@app.route("/create/account", methods="GET")
+@app.route("/create/account", methods=["GET"])
 def create_account():
     return render_template("create_account.html")
 
-@app.route("/create/account", methods="POST")
+@app.route("/create/account", methods=["POST"])
 def create_account_action():
     return render_template("login.html")
 
-@app.route("/logout", methods="GET")
+@app.route("/logout", methods=["GET"])
 @login_required
 def logout():
     return render_template("index.html")
@@ -49,33 +53,42 @@ def logout():
 def review():
     return render_template("reviews.html")
 
-@app.route("/create/review", methods="GET")
+@app.route("/create/review", methods=["GET"])
 @login_required
 def create_review():
     return render_template("create_review.html")
 
-@app.route("/create/review", methods="POST")
+@app.route("/create/review", methods=["POST"])
 @login_required
 def create_review_action():
     return render_template("reviews.html")
 
 
-@app.route("/contact", methods="GET")
+@app.route("/contact", methods=["GET"])
 @login_required
 def contact():
     return render_template("contact.html")
 
-@app.route("/contact", methods="POST")
+@app.route("/contact", methods=["POST"])
 @login_required
 def contact_action():
     return render_template("index.html")
 
-@app.route("/report", methods="GET")
+@app.route("/report", methods=["GET"])
 @login_required
 def report():
     return render_template("report.html")
 
-@app.route("/report", methods="POST")
+@app.route("/report", methods=["POST"])
 @login_required
 def report_action():
     return render_template("index.html")
+
+@app.errorhandler(NotFound)
+def page_not_found(error_message):
+    attempted_url = request.path
+    return render_template("404.html", attempted_url=attempted_url), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8181)
