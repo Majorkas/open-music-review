fc88a7a518ea29a127b773428b9eeed01cb44bcd
Aaron Richards <aaronrichards46@gmail.com>
2025-10-29 18:26:23 +0000

    forgot to add requirements

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+# set actual secret key in .env file
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
-
+#set db URL in .env file too
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
